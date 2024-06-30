421a9ea9fe081917d807916578dac7a0e49d0c47
jk <kampe.johan@gmail.com>
2024-06-30 22:52:17 +0200

    init placeholder project

---
+++ README.md
@@ -0,0 +1,3 @@
+# untappr
+
+Shows info from an Untappd JSON export (retrievable by premium users)
---
+++ src/untappr/__init__.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from untappr")
+
+
+if __name__ == "__main__":
+    main()
